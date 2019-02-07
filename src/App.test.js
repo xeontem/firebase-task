5ff537e28aff7a7c1b680b9a533dc72d87be9b57
Artsiom Kukharev <artsiom_kukharev@epam.com>
2019-02-07 18:45:16 +0300

    functions triggers check

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock('./firebase.service', () => ({
+  fb: { getTodos: () => {} }
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
