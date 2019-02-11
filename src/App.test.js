f2baa9257cb09cd6b76694d000a85326a675c90d
Artsiom Kukharev <artsiom_kukharev@epam.com>
2019-02-11 21:17:25 +0300

    fix: UT fix

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// fb sevice mock
+jest.mock('./firebase.service', () => ({
+  fb: { getTodos: () => {} },
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
